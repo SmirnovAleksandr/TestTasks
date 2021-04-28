326fec97d1c4defa73c0071cbbd1f6442ecb0d51
SmirnovAleksandr <jumpup@mail.ru>
2021-04-28 17:07:26 +0300

    Add ConfigFile for IOS

--- MobileTestTask/Tests/IOS/CoreTestCase.cs
+++ MobileTestTask/Tests/IOS/CoreTestCase.cs
@@ -1,4 +1,6 @@
-﻿using MobileTestTask.Utils;
+﻿using MobileTestTask.Configuration;
+using MobileTestTask.Utils;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
@@ -8,6 +10,7 @@ using OpenQA.Selenium.Remote;
 using SeleniumExtension.Support.Utils;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,29 +21,32 @@ namespace MobileTestTask.Tests.IOS
     public class CoreTestCase
     {
         protected AppiumDriver<IWebElement> Driver;
-        private static string SeleniumHubURL = $"http://172.16.6.188:4723/wd/hub";
+        private static string SeleniumHubURL;
         protected IWait Wait;
+        protected static Config config;
 
-    
         [OneTimeSetUp]
         public void SetUp()
         {
+            var currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var configFile = File.ReadAllText(Path.Combine(currentDirectory, "Configuration.json"));       
+            config = JsonConvert.DeserializeObject< Config>(configFile);
+            
+
+            SeleniumHubURL = config.IOS.Capabilities.Hub;
             var hubUri = new Uri(SeleniumHubURL);
             var capabilities = new AppiumOptions();
-            capabilities.AddAdditionalCapability(MobileCapabilityType.DeviceName, "iPhone8");
-            capabilities.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "14.4.2");
-            capabilities.AddAdditionalCapability(CapabilityType.PlatformName, "IOS");           
-            capabilities.AddAdditionalCapability(MobileCapabilityType.Udid, "a459266f870f38bd0c183a49ba5fbcb3ecd90079");
-            capabilities.AddAdditionalCapability("bundleId", "ru.russianpost.Russian-Post");
-            capabilities.AddAdditionalCapability(MobileCapabilityType.AutomationName, "XCUITest");
-            capabilities.AddAdditionalCapability(MobileCapabilityType.NoReset, "true");
-            capabilities.AddAdditionalCapability("useNewWDA", "false");
+            capabilities.AddAdditionalCapability(MobileCapabilityType.DeviceName, config.IOS.Capabilities.DeviceName);
+            capabilities.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, config.IOS.Capabilities.PlatformVersion);
+            capabilities.AddAdditionalCapability(CapabilityType.PlatformName, config.IOS.Capabilities.PlatformName);           
+            capabilities.AddAdditionalCapability(MobileCapabilityType.Udid, config.IOS.Capabilities.Udid);        
+            capabilities.AddAdditionalCapability("bundleId", config.IOS.Capabilities.BundleId);
+            capabilities.AddAdditionalCapability(MobileCapabilityType.AutomationName, config.IOS.Capabilities.AutomationName);
+            capabilities.AddAdditionalCapability(MobileCapabilityType.NoReset, config.IOS.Capabilities.NoReset);
+            capabilities.AddAdditionalCapability("useNewWDA", config.IOS.Capabilities.UseNewWDA);
             //capabilities.AddAdditionalCapability("xcodeOrgId", "3V72XTL4S5");
             capabilities.AddAdditionalCapability(MobileCapabilityType.NewCommandTimeout, 2400);
-            capabilities.AddAdditionalCapability("language", "en");
-
-            //capabilities.AddAdditionalCapability("bundleId", "by.mts.money");
-
+            capabilities.AddAdditionalCapability(MobileCapabilityType.Language, config.IOS.Capabilities.Language);                     
 
             Driver = new IOSDriver<IWebElement>(new Uri(SeleniumHubURL), capabilities);
             Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
@@ -51,9 +57,6 @@ namespace MobileTestTask.Tests.IOS
                                         typeof(NoSuchElementException)
                                         );
         }
-
-
-
 
     }
 }
--- MobileTestTask/Tests/IOS/Tests.cs
+++ MobileTestTask/Tests/IOS/Tests.cs
@@ -1,9 +1,12 @@
-﻿using MobileTestTask.PageObjects.IOSPageObjects;
+﻿using MobileTestTask.Configuration;
+using MobileTestTask.PageObjects.IOSPageObjects;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium.MultiTouch;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -24,11 +27,12 @@ namespace MobileTestTask.Tests.IOS
         {        
             MainPageObject = new MainPageObject(Driver);
             MorePageObject = new MorePageObject(Driver);
-            User = new User(Driver);
+            User = new User(Driver);        
+
         }
 
         [Test]
-        public void AuthorizeTest() {     
+        public void IOSAuthorizeTest() {     
 
             if (User.IsLoggined()) {
                 User.LogOut();
@@ -38,12 +42,12 @@ namespace MobileTestTask.Tests.IOS
 
 
         [Test]
-        public void SearchProductsTest() {
+        public void IOSSearchProductsTest() {
 
-            MainPageObject.WaitForElementAndClick(MainPageObject.GetMenuElement("Товары"), 10);         
+            MainPageObject.WaitForElementAndClick(MainPageObject.GetMenuElement(config.IOS.IOSSearchProductsTest.MenuElementForClick), 10);         
 
             ProductsPageObject = new ProductsPageObject(Driver);
-            ProductsPageObject.TypeSearchLine("Nutella");
+            ProductsPageObject.TypeSearchLine(config.IOS.IOSSearchProductsTest.TypeSearchLine);
 
             var keyboard = new Keyboard(Driver);
             keyboard.ClickButton(keyboard.Search);
@@ -53,9 +57,9 @@ namespace MobileTestTask.Tests.IOS
         }
 
         [Test]
-        public void SwitchAppToBrowserAndBackToAppTest() {
+        public void IOSSwitchAppToBrowserAndBackToAppTestq() {
 
-            MainPageObject.ClickOnMenuItem("Отделения");
+            MainPageObject.ClickOnMenuItem(config.IOS.IOSSwitchAppToBrowserAndBackToAppTest.MenuElementForClick);
             //В приложении 2-4 баннера в разделе "Ещё". 2 из них точно не кликабельны и в какой-то момент они перестают отображаться \
             // В связи с этим переходим на страницу "Отделение и кликаем на иконку яндекс, в результате чего попадаем в браузер"
 
@@ -64,11 +68,10 @@ namespace MobileTestTask.Tests.IOS
             departmentPageObject.ClickOnYandexLogoByCoordinates(departmentPageObject.smallYandexMap, 360, 235);
 
             departmentPageObject.ClickOnYandexMap(departmentPageObject.bigYandexMap);
-            departmentPageObject.ClickOnYandexLogoByCoordinates(departmentPageObject.smallYandexMap, 365, 490);
-                                               
-            Driver.ActivateApp("ru.russianpost.Russian-Post");
-                       
-        }
+            departmentPageObject.ClickOnYandexLogoByCoordinates(departmentPageObject.bigYandexMap, 365, 490);
 
+            Thread.Sleep(3000);
+            Driver.ActivateApp(config.IOS.Capabilities.BundleId);                       
+        }
     }
 }
